380a2aaa526c9f827d73c300aa3782a9728e9547
Will <asampongg377@gmail.com>
2025-01-08 22:02:58 +0000

    Init

--- portfolio-project/src/Components/Skills.jsx
+++ portfolio-project/src/Components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
       <div className="w-full md:w-4/5 mx-auto flex flex-col items-center">
         <h2 className="font-bold text-4xl mt-5 mb-20 text-center">Specialized In</h2>
         <div className="flex items-center justify-center md:gap-x-6 gap-y-4 flex-wrap mb-10 md:mb-0">
-          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white">
+          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white shadow-md">
             <div className="web-icon bg-[#F5F3FE] p-4 rounded-lg">
               <GlobeSVG />
             </div>
@@ -26,8 +26,16 @@ const Skills = () => {
             <div className="desc">
               <p className="text-wrap text-sm text-center">Building dynamic, responsive, and accessible websites using modern technologies.</p>
             </div>
+            <div className="tools flex gap-x-2">
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">HTML</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">CSS</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">JS</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">React</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">Tailwind</span>
+            </div>
           </div>
-          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white">
+          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white shadow-md
+          ">
             <div className="web-icon bg-[#F5F3FE] p-4 rounded-lg">
               <AnalyticsSVG />
             </div>
@@ -37,8 +45,12 @@ const Skills = () => {
             <div className="desc">
               <p className="text-wrap text-sm text-center">Analyzing complex datasets to provide informed insights for decision-making.</p>
             </div>
+            <div className="tools flex gap-x-2">
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">Python</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">R</span>
+            </div>
           </div>
-          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white">
+          <div className="skill-card md:flex-1 flex flex-col items-center gap-y-4 py-10 px-5 rounded-md bg-white shadow-md">
             <div className="web-icon bg-[#F5F3FE] p-4 rounded-lg">
               <GitSVG1 />
             </div>
@@ -47,6 +59,10 @@ const Skills = () => {
             </div>
             <div className="desc">
               <p className="text-wrap text-sm text-center">Managing codebases and collaborating efficiently using Git and GitHub.</p>
+            </div>
+            <div className="tools flex gap-x-2">
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">Git</span>
+              <span className="bg-[#7E74F1] rounded py-1 px-2 shadow-sm text-white text-xs">Github</span>
             </div>
           </div>
         </div>
